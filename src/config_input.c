1418c58abc7444ba663e6665bad362b12deddc8c
codewitch-honey-crisis <gaztoast@gmail.com>
2025-12-28 14:01:41 -0800

    fixed touch input

--- src/config_input.c
+++ src/config_input.c
@@ -6,21 +6,12 @@
 #include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#ifdef BUTTON
-static TickType_t pressed_ts = 0;
-#endif
-#ifdef TOUCH_BUS
-static TickType_t touched_ts = 0;
-#endif
+
 void config_input_update(void) {
 #ifdef BUTTON
-    if(!panel_button_read_all()) {
-        pressed_ts = xTaskGetTickCount();
-    } else {
-        if(xTaskGetTickCount()>pressed_ts+pdMS_TO_TICKS(250)) {
-            config_add_value("configure","");
-            esp_restart();
-        }
+    if(panel_button_read_all()) {    
+        config_add_value("configure","");
+        esp_restart();
     }
 #endif
 #ifdef TOUCH_BUS
@@ -28,13 +19,11 @@ void config_input_update(void) {
     uint16_t x,y,s;
     size_t count = 1;
     panel_touch_read_raw(&count,&x,&y,&s);
-    if(!count) {
-        touched_ts = xTaskGetTickCount();
-    } else {
-        if(xTaskGetTickCount()>touched_ts+pdMS_TO_TICKS(250)) {
-            config_add_value("configure","");
-            esp_restart();
-        }
+    if(count) {
+       
+        config_add_value("configure","");
+        esp_restart();
     }
+
 #endif
 }
